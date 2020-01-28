18a90aee885ddd1457fb2eba0426ce646b6d82a8
leonidstromov <stromovleonid@gmail.com>
2020-01-28 12:54:52 +0700

    login module created

--- feature-login/src/main/java/com/example/feature_login/LoginActivity.kt
+++ feature-login/src/main/java/com/example/feature_login/LoginActivity.kt
@@ -1,12 +1,12 @@
-package com.example.khotos_android
+package com.example.feature_login
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
