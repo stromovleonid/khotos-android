69f55c2f6df1ddc7a85c52f5ceecc51d5d1ce630
leonidstromov <stromovleonid@gmail.com>
2020-02-03 21:59:28 +0700

    photos model refactoring

--- feature-photos/src/main/java/com/example/feature_photos/feed/model/PhotosFeedModel.kt
+++ feature-photos/src/main/java/com/example/feature_photos/feed/model/PhotosFeedModel.kt
@@ -1,29 +1,17 @@
 package com.example.feature_photos.feed.model
 
-import com.example.common.mvi.model.BaseModel
 import com.example.data.repositories.PhotosRepository
 import com.example.data.utils.DispatchersProvider
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.map
 
-@ExperimentalCoroutinesApi
 @FlowPreview
-open class PhotosFeedModel(
+@ExperimentalCoroutinesApi
+class PhotosFeedModel(
     initialState: PhotosFeedModelState,
-    private val photosRepository: PhotosRepository,
+    photosRepository: PhotosRepository,
     dispatchersProvider: DispatchersProvider
-) : BaseModel<PhotosFeedModelState>(initialState, dispatchersProvider) {
-
-    override fun submitState(newState: PhotosFeedModelState) = Unit
-
-    override fun observe() = photosRepository.getPhotosFeed().map {
-        PhotosFeedModelState(
-            it,
-            false,
-            0,
-            false
-
-        )
-    }
+) : AbstractPhotosFeedModel(initialState, photosRepository, dispatchersProvider) {
+    override fun getPhotosFlow(photosRepository: PhotosRepository) = photosRepository.getPhotosFeed()
 }
+
